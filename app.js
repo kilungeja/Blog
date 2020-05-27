d4e69618269d7847c99be6e08a9ad4af300c4f8b
kilungeja <tumpalenazarius@gmail.com>
2020-05-27 01:18:49 -0400

    added a database connection and user model

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require("express");
-
-const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-
-app.use("/", (req, res) => {
-  const users = [
-    { name: "Jane", age: 12 },
-    { name: "John", age: 22 },
-    { name: "Janeth", age: 72 }
-  ];
-  res.status(200).json({ users: users });
-});
-
-app.listen(9090, () => {
-  console.log("Server listening on port 9090");
-});
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log("something went wrong");
+  });
